8e219afb9be97ca6ee1de4b88f738772f03f805a
Nicolas <nicolasgesielfg@gmail.com>
2026-06-13 22:24:12 -0300

    chore: setup inicial do Spring Boot 4.0.7

---
+++ src/main/java/com/nicolas/paises_api/PaisesApiApplication.java
@@ -0,0 +1,13 @@
+package com.nicolas.paises_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaisesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaisesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicolas/paises_api/PaisesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolas.paises_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaisesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
